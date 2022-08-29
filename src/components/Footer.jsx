ddc3a062ef3e9759636be71d03fab6d682ca789e
Xyzuka <yeebrandon@outlook.com>
2022-08-28 23:54:08 -0700

    adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import '../App.css'
+import twitterIcon from '../assets/Twitter-Icon.png'
+import facebookIcon from '../assets/Facebook-Icon.png'
+import instagramIcon from '../assets/Instagram-Icon.png'
+import githubIcon from '../assets/Github-Icon.png'
 
 export default function Footer() {
   return (
-    <footer>test</footer>
+    <div className='footer-container'>
+      <img src={twitterIcon} alt="twitter-icon" className='icon'/>
+      <img src={facebookIcon} alt="facebook-icon" className='icon'/>
+      <img src={instagramIcon} alt="facebook-icon" className='icon'/>
+      <img src={githubIcon} alt="facebook-icon" className='icon'/>
+    </div>
+
   )
 }
